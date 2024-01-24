da4bd63703284846190c506b9cc79de44424e32e
Shade-v2006 <Shade-v2006@yandex.ru>
2024-01-25 04:24:17 +0700

    add Lesson1:homework

---
+++ HomeWork_1/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 3;
+int b = 6;
+int c = 4;
+double d = 8;
+
+//int result = (a * b) / (c + d);
+//Console.WriteLine(result);
+
+
+double result2 = (a * b) / (c + d);
+Console.WriteLine(result2);
