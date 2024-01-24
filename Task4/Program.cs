bced80b6d1c279aa45f496400f975aea5d4d6e14
Shade-v2006 <Shade-v2006@yandex.ru>
2024-01-25 05:19:23 +0700

    add lesson1:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 23;
+int b = 6;
+int c = 22;
+int d = 8;
+int e = 7;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
